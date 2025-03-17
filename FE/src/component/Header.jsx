53d517b4a97c6dbb4a8459625bac2b5edd8edf3a
bjh0499 <j9603h@gmail.com>
2025-03-17 11:48:52 +0900

    [P24-18] chore: 전체적인 레이아웃, 메인 페이지 생성

--- FE/src/component/Header.jsx
+++ FE/src/component/Header.jsx
@@ -1,3 +1,21 @@
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="h-full">
+      <div className="flex justify-between h-full p-2">
+        <div className="flex flex-row flex-6 font-bold">
+          <div className="flex items-center p-4 max-w-40">
+            <img src="/logo.png" className="" />
+          </div>
+
+          {/* <div className="flex items-center p-4 text-2xl text-primary">체크리스트</div>
+          <div className="flex items-center p-4 text-2xl">캘린더</div> */}
+        </div>
+
+        <div className="flex flex-row flex-4 justify-end">
+          {/* <div className="flex items-center p-4 text-2xl text-secondary">이사 플랜 1</div>
+          <div className="flex items-center p-4 text-2xl">로그아웃</div> */}
+        </div>
+      </div>
+    </div>
+  );
 }
--- FE/src/pages/Home.jsx
+++ FE/src/pages/Home.jsx
@@ -1,3 +1,24 @@
+import { Link } from 'react-router-dom';
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="flex justify-center item-center h-full">
+      <div className="flex flex-col justify-center item-center">
+        <div className="flex justify-center item-center text-6xl font-extrabold p-8">
+          흩어져 있는 <span>&nbsp;</span>
+          <span className="text-primary">이사</span>의 모든 것
+        </div>
+        <div className="flex justify-center item-center text-4xl p-8">
+          막막한 이사, 이사모음.zip과 깐깐하게 함께 해요!
+        </div>
+        <div className="flex justify-center item-center p-12">
+          <Link to="/login">
+            <button className="w-60 h-20 text-white text-4xl bg-primary rounded-3xl cursor-pointer font-bold">
+              시작하기
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
