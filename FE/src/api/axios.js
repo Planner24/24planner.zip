0ef195d8c8b73700be8424fb05cf3c1218db80f2
apricity2u <gkdms9849@gmail.com>
2025-03-17 17:55:29 +0900

    [P24-15] feat: withCredentials 옵션 적용 및 요청 인터셉터 추가
    - withCredentials 옵션을 통해 refreshToken을 브라우저가 자동으로
      전달하게 해줌
    - 요청 인터셉터에서 accessToken을 header에 넣어줌

--- FE/src/api/axios.js
+++ FE/src/api/axios.js
@@ -1,7 +1,22 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: '/api',
+  withCredentials: true, // 쿠키를 요청에 포함
 });
+
+// 요청 인터셉터
+api.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem('accessToken');
+    if (accessToken) {
+      // accessToken을 Authorization 헤더에 포함시킴
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  // 에러를 거절 상태로 반환하여, 이후 처리를 catch 등의 방식으로 할 수 있게 함
+  (error) => Promise.reject(error),
+);
 
 export default api;
