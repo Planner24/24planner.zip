61ae19c6de66f3ed0a4e463a09d39bdedc743273
cocobabb <cocobab05108@gmail.com>
2025-03-18 02:20:39 +0900

    [P24-22] feat: SMTP 및 Redis 활용한 이메일 인증 구현
    - 사용자가 입력한 이메일로 인증코드 전송
    - 가입된 이메일이면 인증 코드 전송 안되게 예외처리
    - Redis에 인증 코드 저장
    - 인증코드 만료일 응답

--- BE/src/main/java/com/example/p24zip/domain/user/controller/AuthController.java
+++ BE/src/main/java/com/example/p24zip/domain/user/controller/AuthController.java
@@ -1,15 +1,19 @@
 package com.example.p24zip.domain.user.controller;
 
 import com.example.p24zip.domain.user.dto.request.SignupRequestDto;
+import com.example.p24zip.domain.user.dto.request.VerifyEmailRequestDto;
+import com.example.p24zip.domain.user.dto.response.VerifyEmailDataResponseDto;
 import com.example.p24zip.domain.user.service.AuthService;
 import com.example.p24zip.global.response.ApiResponse;
 import jakarta.validation.Valid;
+import java.util.Random;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequiredArgsConstructor
@@ -26,5 +30,18 @@ public class AuthController {
         return ResponseEntity.ok(
             ApiResponse.ok("CREATED", "회원가입을 성공했습니다.", null)
         );
+    }
+
+    @PostMapping(value = "/verify-email")
+    public ResponseEntity<ApiResponse<VerifyEmailDataResponseDto>> verifyEmail(@RequestBody @Valid VerifyEmailRequestDto requestDto){
+        String subject = "회원가입 인증 메일입니다.";
+        Random random = new Random();
+        int code = random.nextInt(9000) +1000;
+        String text = "인증 코드는" + code + "입니다.";
+
+        return ResponseEntity.ok(
+            ApiResponse.ok("OK", "인증 번호를 전송했습니다.", authService.sendEmail(requestDto.getUsername(), subject, text, code))
+        );
+
     }
 }
--- BE/src/main/java/com/example/p24zip/domain/user/service/AuthService.java
+++ BE/src/main/java/com/example/p24zip/domain/user/service/AuthService.java
@@ -2,11 +2,22 @@ package com.example.p24zip.domain.user.service;
 
 
 import com.example.p24zip.domain.user.dto.request.SignupRequestDto;
+import com.example.p24zip.domain.user.dto.response.VerifyEmailDataResponseDto;
 import com.example.p24zip.domain.user.entity.User;
 import com.example.p24zip.domain.user.repository.UserRepository;
 import com.example.p24zip.global.exception.CustomException;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+
+import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,8 +28,14 @@ import org.springframework.transaction.annotation.Transactional;
 public class AuthService {
 
     private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
+    private final PasswordEncoder passwordEncoder; // 회원가입 시 비밀번호 암호화
+    private final JavaMailSender mailSender; // 메일 보내는 객체
+    private final StringRedisTemplate redisTemplate; // redis 객체
 
+    /**
+     * @param requestDto
+     *    username(email), password, nickname
+     * **/
     @Transactional
     public void signup(@Valid SignupRequestDto requestDto) {
         boolean checkUsername = checkExistsUsername(requestDto.getUsername());
@@ -33,7 +50,51 @@ public class AuthService {
         userRepository.save(user);
     }
 
+    /**
+     * @param userName 입력한 email
+     * @return Boolean 이메일 존재 유무
+     * **/
     public boolean checkExistsUsername(String userName) {
         return userRepository.existsByUsername(userName);
     }
+
+    /**
+     * @param username 입력한 email
+     * @param subject 보내질 이메일 제목
+     * @param text 보내질 이메일 본문 내용
+     * @param code 보내질 인증 코드 랜덤한 4자리 수
+     * @return 만료일 가진 responseDto
+     * 
+     * **/
+    public VerifyEmailDataResponseDto sendEmail(@NotNull @Email String username, String subject, String text, int code) {
+        boolean checkUsername = checkExistsUsername(username);
+        if (checkUsername) {
+            throw new CustomException("EXIST_EMAIL", "이미 사용중인 이메일입니다.");
+        }
+
+        // 정의된 SMTP 메일 객체로 메일 전송
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(username);
+        message.setSubject(subject);
+        message.setText(text);
+        mailSender.send(message);
+        
+        // redis인증 코드 저장
+        LocalDateTime expiredAt = saveCodeToRedis(username, String.valueOf(code));
+
+        return VerifyEmailDataResponseDto.from(expiredAt);
+
+
+    }
+    /**
+     * @param username 입력한 email
+     * @param code 4자리의 랜덤 수
+     * @return LocalDateTime expiredAt
+     * **/
+    public LocalDateTime saveCodeToRedis(String username, String code) {
+
+        redisTemplate.opsForValue().set(username, code, 3, TimeUnit.MINUTES);
+        return LocalDateTime.now().plusMinutes(3);
+    }
+
 }
