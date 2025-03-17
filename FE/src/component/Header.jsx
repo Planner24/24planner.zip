763f65e923ff05be3fb9f45361ab65ccf3bf714f
bjh0499 <j9603h@gmail.com>
2025-03-17 12:27:31 +0900

    [P24-18] refactor: 불필요한 Tailwind CSS 요소 삭제 후, 중복되는 부분을 변수화

--- FE/src/component/Header.jsx
+++ FE/src/component/Header.jsx
@@ -1,21 +1,24 @@
 export default function Header() {
+  const headerItemStyle = 'flex items-center p-4';
+
   return (
     <div className="h-full">
-      <div className="flex justify-between h-full p-2">
-        <div className="flex flex-row flex-6 font-bold">
-          <div className="flex items-center p-4 max-w-40">
-            <img src="/logo.png" className="" />
+      <div className="flex justify-between h-full p-2 text-2xl">
+        {/* Linux 환경 Firefox에서 flex에 이미지가 들어가면 무조건 크기를 최대로 차지하는 문제가 있어 flex 크기 수동 지정*/}
+        <div className="flex flex-4 font-bold">
+          <div className={`${headerItemStyle} max-w-40`}>
+            <img src="/logo.png" />
           </div>
 
-          {/* 로그인 후 화면까지 구현한 시점에서 활성화 예정 /*}
-          {/* <div className="flex items-center p-4 text-2xl text-primary">체크리스트</div>
-          <div className="flex items-center p-4 text-2xl">캘린더</div> */}
+          {/* 로그인 후 화면까지 구현한 시점에서 활성화 예정 */}
+          <div className={`${headerItemStyle} text-primary`}>체크리스트</div>
+          <div className={`${headerItemStyle}`}>캘린더</div>
         </div>
 
-        <div className="flex flex-row flex-4 justify-end">
-          {/* 로그인 후 화면까지 구현한 시점에서 활성화 예정 /*}
-          {/* <div className="flex items-center p-4 text-2xl text-secondary">이사 플랜 1</div>
-          <div className="flex items-center p-4 text-2xl">로그아웃</div> */}
+        <div className="flex flex-6 justify-end">
+          {/* 로그인 후 화면까지 구현한 시점에서 활성화 예정 */}
+          <div className={`${headerItemStyle} text-secondary`}>이사 플랜 1</div>
+          <div className={`${headerItemStyle}`}>로그아웃</div>
         </div>
       </div>
     </div>
