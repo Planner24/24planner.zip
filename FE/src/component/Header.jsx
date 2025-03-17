59846c98e0b132f67cf9ab3636c5c327be7c3e7c
bjh0499 <j9603h@gmail.com>
2025-03-17 11:57:22 +0900

    [P24-18] refactor: 추후 진행에 따라 다시 활성화할 코드에 대해 주석 추가

--- FE/src/component/Header.jsx
+++ FE/src/component/Header.jsx
@@ -7,11 +7,13 @@ export default function Header() {
             <img src="/logo.png" className="" />
           </div>
 
+          {/* 로그인 후 화면까지 구현한 시점에서 활성화 예정 /*}
           {/* <div className="flex items-center p-4 text-2xl text-primary">체크리스트</div>
           <div className="flex items-center p-4 text-2xl">캘린더</div> */}
         </div>
 
         <div className="flex flex-row flex-4 justify-end">
+          {/* 로그인 후 화면까지 구현한 시점에서 활성화 예정 /*}
           {/* <div className="flex items-center p-4 text-2xl text-secondary">이사 플랜 1</div>
           <div className="flex items-center p-4 text-2xl">로그아웃</div> */}
         </div>
