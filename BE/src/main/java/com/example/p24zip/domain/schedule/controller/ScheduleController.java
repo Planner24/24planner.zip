85805c6bc0bcd1f7fa29c7e953e7a3edb911cae7
apricity2u <gkdms9849@gmail.com>
2025-03-20 18:12:00 +0900

    [P24-43] feat: 할 일 전체 조회 기능 구현

--- BE/src/main/java/com/example/p24zip/domain/schedule/controller/ScheduleController.java
+++ BE/src/main/java/com/example/p24zip/domain/schedule/controller/ScheduleController.java
@@ -1,16 +1,20 @@
 package com.example.p24zip.domain.schedule.controller;
 
 import com.example.p24zip.domain.schedule.dto.request.ScheduleCreateRequestDto;
+import com.example.p24zip.domain.schedule.dto.response.ScheduleListResponseDto;
 import com.example.p24zip.domain.schedule.dto.response.ScheduleResponseDto;
 import com.example.p24zip.domain.schedule.service.ScheduleService;
 import com.example.p24zip.global.response.ApiResponse;
 import jakarta.validation.Valid;
+import java.time.YearMonth;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -30,6 +34,18 @@ public class ScheduleController {
             "CREATED",
             "할 일이 생성되었습니다.",
             scheduleService.createSchedule(requestDto, movingPlanId)
+        ));
+    }
+
+    // 할 일 전체 조회
+    @GetMapping("/month")
+    public ResponseEntity<ApiResponse<ScheduleListResponseDto>> getSchedules(
+        @PathVariable Long movingPlanId,
+        @RequestParam YearMonth month){
+        return ResponseEntity.ok(ApiResponse.ok(
+            "OK",
+            "할 일 목록 조회에 성공했습니다.",
+            scheduleService.getSchedules(movingPlanId, month)
         ));
     }
 
--- BE/src/main/java/com/example/p24zip/domain/schedule/service/ScheduleService.java
+++ BE/src/main/java/com/example/p24zip/domain/schedule/service/ScheduleService.java
@@ -3,11 +3,13 @@ package com.example.p24zip.domain.schedule.service;
 import com.example.p24zip.domain.movingPlan.entity.MovingPlan;
 import com.example.p24zip.domain.movingPlan.repository.MovingPlanRepository;
 import com.example.p24zip.domain.schedule.dto.request.ScheduleCreateRequestDto;
+import com.example.p24zip.domain.schedule.dto.response.ScheduleListResponseDto;
 import com.example.p24zip.domain.schedule.dto.response.ScheduleResponseDto;
 import com.example.p24zip.domain.schedule.entity.Schedule;
 import com.example.p24zip.domain.schedule.repository.ScheduleRepository;
-import com.example.p24zip.domain.user.entity.User;
 import com.example.p24zip.global.exception.ResourceNotFoundException;
+import java.time.YearMonth;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -32,5 +34,26 @@ public class ScheduleService {
         Schedule newSchedule = scheduleRepository.save(requestDto.toEntity(movingPlan));
 
         return ScheduleResponseDto.from(newSchedule);
+    }
+
+    // 할 일 전체 조회
+    public ScheduleListResponseDto getSchedules(Long movingPlanId, YearMonth month){
+
+        List<Schedule> allSchedules = scheduleRepository.findAllByMovingPlanId(movingPlanId);
+
+        int monthValue = month.getMonthValue();
+
+        List<ScheduleResponseDto> scheduleInMonth = allSchedules.stream()
+            .filter(schedule -> isScheduleInMonth(schedule, monthValue))
+            .map(ScheduleResponseDto :: from)
+            .toList();
+
+        return ScheduleListResponseDto.from(month, scheduleInMonth);
+    }
+
+    // 검색한 달의 스케줄인지 확인
+    private boolean isScheduleInMonth(Schedule schedule, int monthValue){
+        return schedule.getStartDate().getMonthValue() == monthValue
+            || schedule.getEndDate().getMonthValue() == monthValue;
     }
 }
