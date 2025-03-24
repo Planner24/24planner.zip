0c2fcdfe211bb316c2d3de5418397a5b7628eda4
KoHo512 <sesac.yw@gmail.com>
2025-03-24 17:59:27 +0900

    [P24-70] style: 이사 목록 페이지 기본 틀 구현
    - RootLayout의 Outlet을 감싸는 div에 적용된 h-4/5 클래스 때문에 footer가 엉뚱한 위치에 고정되는 문제가 발생해 제거하고 진행하기로 함.

--- FE/src/pages/PlanList.jsx
+++ FE/src/pages/PlanList.jsx
@@ -1,10 +1,19 @@
 import authApi from '../api/authApi';
 
 export default function PlanList() {
-  // 테스트
-  const onClick = async () => {
-    await authApi.test();
-  };
+  const displayStyle = 'px-3 py-10 flex flex-col items-center gap-10 text-center';
+  const planHeader = 'w-full';
+  const titleStyle = 'text-2xl';
+  const lineStyle = 'w-9/10 mx-auto mt-5 border-t-2 border-primary';
+  const planDiv = 'px-30';
 
-  return <button onClick={onClick}>test</button>;
+  return (
+    <div className={displayStyle}>
+      <div className={planHeader}>
+        <h2 className={titleStyle}>이사 목록</h2>
+        <hr className={lineStyle} />
+      </div>
+      <div className={planDiv}></div>
+    </div>
+  );
 }
