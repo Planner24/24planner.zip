3f3df99e05fc9f7d60a8f335ce29c3e53a532fda
KoHo512 <sesac.yw@gmail.com>
2025-03-25 17:21:16 +0900

    [P24-70] feat: 이사 플랜 생성 구현
    - 플랜 생성 컴포넌트 구현
        - 생성 컴포넌트 클릭 시, 플랜 추가를 위한 입력란 활성화
        - 추가 버튼을 클릭하거나 엔터를 입력한 경우 플랜이 생성되도록 구현
        - 생성 컴포넌트 외부 클릭 시, 입력란 비활성화
        - 새 플랜 생성 시, plan 리스트에 추가되어 렌더링되도록 구현
    - 플랜 컴포넌트 태그 수정 및 CSS 보완
    - 이사 목록 페이지 CSS 보완
    - RootLayout의 w-screen 클래스 때문에 세로 스크롤이 생기는 경우, 가로 스크롤이 생겨 수정하기로 함

--- FE/src/component/plan/Plan.jsx
+++ FE/src/component/plan/Plan.jsx
@@ -1,20 +1,28 @@
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 export default function Plan({ plan }) {
+  const navigate = useNavigate();
+
   const { id, title } = plan;
 
   // CSS
   const planLi =
-    'w-full flex items-center justify-between mb-10 px-5 py-5 border-2 border-primary rounded-3xl relative';
+    'w-full h-20 flex items-center justify-center mb-10 py-5 border-2 border-primary rounded-3xl relative cursor-pointer';
   const planText = 'w-full text-center text-2xl ';
   const settingLink = 'absolute right-5 text-xl cursor-pointer';
 
   return (
-    <li className={planLi}>
-      <div className={planText}>{title}</div>
-      <Link to={`/plans/${id}/setting`} className={settingLink}>
+    <li className={planLi} onClick={() => navigate(`/plans/${id}`)}>
+      <p className={planText}>{title}</p>
+      <button
+        className={settingLink}
+        onClick={(e) => {
+          e.stopPropagation();
+          navigate(`/plans/${id}/setting`);
+        }}
+      >
         ⚙️
-      </Link>
+      </button>
     </li>
   );
 }
--- FE/src/pages/PlanList.jsx
+++ FE/src/pages/PlanList.jsx
@@ -1,28 +1,36 @@
 import { useEffect, useState } from 'react';
 import planApi from '../api/planApi';
 import Plan from '../component/plan/Plan';
+import PlanCreator from '../component/plan/PlanCreator';
 
 export default function PlanList() {
+  // 상태 관리 데이터
   const [plans, setPlans] = useState([]);
 
+  // 이사 플랜 리스트 조회
   useEffect(() => {
     async function fetchPlans() {
       try {
         const response = await planApi.readPlans();
         const data = response.data.data;
-        console.log(data.movingPlans);
+
         setPlans(data.movingPlans);
       } catch (error) {}
     }
     fetchPlans();
   }, []);
 
+  // 새 이사 플랜 생성 시 리스트에 추가
+  const handleNewPlan = (newPlan) => {
+    setPlans((prevPlans) => [newPlan, ...prevPlans]);
+  };
+
   // CSS
   const displayStyle = 'px-3 py-10 flex flex-col items-center gap-10 text-center';
   const planHeader = 'w-full';
   const titleStyle = 'text-2xl';
-  const lineStyle = 'w-9/10 mx-auto mt-5 border-t-2 border-primary';
-  const planDiv = 'w-full px-60 py-10 list-none';
+  const lineStyle = 'mx-20 mt-5 border-t-2 border-primary';
+  const planDiv = 'w-full px-60 py-10 list-none max-w-full';
 
   return (
     <div className={displayStyle}>
@@ -31,8 +39,9 @@ export default function PlanList() {
         <hr className={lineStyle} />
       </div>
       <ul className={planDiv}>
+        <PlanCreator onPlanCreated={handleNewPlan} />
         {plans.map((plan) => (
-          <Plan plan={plan} />
+          <Plan key={plan.id} plan={plan} />
         ))}
       </ul>
     </div>
