61fbb9d86ff1a4a88f277339a1b380af4c962189
KoHo512 <sesac.yw@gmail.com>
2025-03-25 22:29:07 +0900

    [P24-77] feat: 이사 플랜 설정 페이지 기본 틀 구현
    - planApi에 플랜 조회 api 연결
    - api를 통해 조회해온 제목을 화면에 렌더링
    - 제목 수정 및 플랜 삭제 버튼 위치 세팅

--- FE/src/pages/PlanSetting.jsx
+++ FE/src/pages/PlanSetting.jsx
@@ -1,3 +1,43 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import planApi from '../api/planApi';
+
 export default function PlanSetting() {
-  return <div>PlanSetting</div>;
+  const { movingPlanId } = useParams();
+
+  // 상태 관리 데이터
+  const [plan, setPlan] = useState();
+
+  // 이사 플랜 조회
+  useEffect(() => {
+    async function fetchPlan() {
+      try {
+        const response = await planApi.readPlan(movingPlanId);
+        const data = response.data.data;
+
+        setPlan(data);
+      } catch (error) {}
+    }
+    fetchPlan();
+  });
+
+  // CSS
+  const displayStyle = 'w-300 mx-auto my-5 text-center';
+  const titleHeader = 'h-full mx-60 flex justify-between';
+  const titleDiv = 'flex';
+  const titleStyle = 'text-2xl mr-5';
+  const titleButton =
+    'text-xl text-gray-500 text-opacity-70 underline cursor-pointer hover:text-primary';
+
+  return (
+    <div className={displayStyle}>
+      <div className={titleHeader}>
+        <div className={titleDiv}>
+          <h2 className={titleStyle}>{plan?.title}</h2>
+          <button className={titleButton}>수정</button>
+        </div>
+        <button className={titleButton}>이사 플랜 삭제</button>
+      </div>
+    </div>
+  );
 }
