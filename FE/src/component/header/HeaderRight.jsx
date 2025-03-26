76674f9fcb0fed5f58a0eef16fd49ff0b35d4407
bjh0499 <j9603h@gmail.com>
2025-03-26 19:15:10 +0900

    [P24-83] feat: 헤더에 현재 선택한 이사 플랜 이름이 뜨도록 수정

--- FE/src/component/header/HeaderRight.jsx
+++ FE/src/component/header/HeaderRight.jsx
@@ -1,14 +1,43 @@
+import { useState, useEffect } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { logout } from '../../store/slices/authSlice';
 import authApi from '../../api/authApi';
+import planApi from '../../api/planApi';
 
 export default function HeaderRight() {
   const location = useLocation();
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
+  const [storedPlanId, setStoredPlanId] = useState(0);
+  const [currentPlanTitle, setCurrentPlanTitle] = useState('');
+
   const splitedUrlString = location.pathname.split('/');
+  let currentPlanId = splitedUrlString[2];
+  currentPlanId = currentPlanId === undefined ? 0 : currentPlanId;
+
+  if (currentPlanId != storedPlanId) {
+    setStoredPlanId(currentPlanId);
+  }
+
+  useEffect(() => {
+    async function setPlanTitle() {
+      try {
+        const response = await planApi.readPlan(storedPlanId);
+        setCurrentPlanTitle(response.data.data.title);
+      } catch (err) {
+        console.log(err);
+        setCurrentPlanTitle('');
+      }
+    }
+
+    if (storedPlanId) {
+      setPlanTitle();
+    } else {
+      setCurrentPlanTitle('');
+    }
+  }, [storedPlanId]);
 
   const handleLogoutClick = async () => {
     dispatch(logout());
@@ -18,8 +47,8 @@ export default function HeaderRight() {
 
   const headerListStyle = 'flex flex-1 justify-end';
   const headerItemStyle = 'flex items-center p-4';
-  const headerDropdownStyle = headerItemStyle + ' relative group';
-  const headerDropdownButtonStyle = 'text-secondary cursor-pointer';
+  const headerDropdownStyle = headerItemStyle + ' relative group min-w-30';
+  const headerDropdownButtonStyle = 'w-full text-center text-secondary cursor-pointer';
   const headerDropdownBodyStyle =
     'absolute text-xl text-center top-15 space-y-4 left-0 right-0 w-full py-4 bg-gray-100 shadow-sm opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-300';
   const headerDropdownItemStyle = 'w-full';
@@ -32,7 +61,7 @@ export default function HeaderRight() {
       splitedUrlString[2] &&
       Number.parseInt(splitedUrlString[2]) > 0 ? (
         <li className={headerDropdownStyle}>
-          <span className={headerDropdownButtonStyle}>이사 플랜 이름</span>
+          <div className={headerDropdownButtonStyle}>{currentPlanTitle}</div>
           <ul className={headerDropdownBodyStyle}>
             <li className={headerDropdownItemStyle}>
               <Link to="/plans" className={headerDropdownLinkStyle}>
