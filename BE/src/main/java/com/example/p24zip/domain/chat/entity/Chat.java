3b2dfb5ce0c804d69c78123e30bc3062388dc3b8
rkdrudgns0412 <rkdrudgns8233@gmail.com>
2025-03-27 11:37:11 +0900

    [P24-89] feat: chat entity 추가

--- BE/src/main/java/com/example/p24zip/domain/chat/entity/Chat.java
+++ BE/src/main/java/com/example/p24zip/domain/chat/entity/Chat.java
@@ -1,4 +1,25 @@
 package com.example.p24zip.domain.chat.entity;
 
-public class Chat {
+
+import com.example.p24zip.domain.movingPlan.entity.MovingPlan;
+import com.example.p24zip.global.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Chat extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @ManyToOne
+    @JoinColumn(name="moving_plan_id")
+    private MovingPlan movingPlan;
+
+    private Long text;
 }
