d8062a4726c8076dfb09fbadd911861d38674ce2
bjh0499 <j9603h@gmail.com>
2025-03-31 16:36:22 +0900

    [P24-97] fix: 달력과 일별 일정 목록의 일정 순서가 일치하도록 수정

--- FE/src/component/calendar/CalendarSidebar.jsx
+++ FE/src/component/calendar/CalendarSidebar.jsx
@@ -26,6 +26,34 @@ export default function CalendarSidebar({
     setDailyScheduleList(() => []);
     try {
       const response = await scheduleApi.getDailySchedule(movingPlanId, selectDate);
+      const schedules = response.data.data.schedules;
+      schedules.sort((date1, date2) => {
+        const startDateInt1 = calendarUtil.parseIntFromDateStr(date1.startDate);
+        const startDateInt2 = calendarUtil.parseIntFromDateStr(date2.startDate);
+
+        if (startDateInt1 != startDateInt2) {
+          return startDateInt1 - startDateInt2;
+        }
+
+        const endDateInt1 = calendarUtil.parseIntFromDateStr(date1.endDate);
+        const endDateInt2 = calendarUtil.parseIntFromDateStr(date2.endDate);
+
+        if (endDateInt1 != endDateInt2) {
+          return endDateInt2 - endDateInt1;
+        }
+
+        const content1 = date1.content;
+        const content2 = date2.content;
+
+        if (content1 < content2) {
+          return -1;
+        } else if (content1 > content2) {
+          return 1;
+        }
+
+        return 0;
+      });
+
       setDailyScheduleList(() => response.data.data.schedules);
     } catch (err) {
       console.log(err);
