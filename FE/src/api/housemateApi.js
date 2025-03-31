67ff6df0fa63cee39b4353f1663c53fb5cf6a7a4
KoHo512 <sesac.yw@gmail.com>
2025-03-31 17:08:27 +0900

    [P24-94] feat: 이사 설정 페이지 동거인 초대 기능 구현
    - 이사 플랜 초대하기 버튼 클릭하면 초대 링크 생성
    - 복사 버튼 누르면 초대 링크가 클립보드에 복사됨

--- FE/src/api/housemateApi.js
+++ FE/src/api/housemateApi.js
@@ -3,6 +3,12 @@ import api from './axios';
 const ENDPOINT = '/plans';
 
 const housemateApi = {
+  // 동거인 초대 링크 생성
+  createInvitationLink: async (movingPlanId) => {
+    const response = await api.post(`${ENDPOINT}/${movingPlanId}/housemates/invite`);
+    return response;
+  },
+
   // 동거인 삭제
   deleteHousemate: async (movingPlanId, housemateId) => {
     const response = await api.delete(`${ENDPOINT}/${movingPlanId}/housemates/${housemateId}`);
