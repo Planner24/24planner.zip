2d511872aa7c52fdd93ca0a46f31d590ea93955a
apricity2u <gkdms9849@gmail.com>
2025-04-01 22:20:44 +0900

    [P24-96] test: env 삭제 후 소셜로그인 배포 환경 3차 테스트

--- FE/src/component/login/KakaoLogin.jsx
+++ FE/src/component/login/KakaoLogin.jsx
@@ -2,8 +2,12 @@ import kakaoLogin from '../../assets/kakaoLogin.png';
 
 export default function KakaoLogin() {
   const handleSocialLogin = () => {
-    const apiUrl = import.meta.env.VITE_BASE_URL;
-    window.location.href = `${apiUrl}/api/oauth2/authorization/kakao`;
+    // 로컬 환경
+    // const apiUrl = import.meta.env.VITE_BASE_URL;
+    // window.location.href = `${apiUrl}/api/oauth2/authorization/kakao`;
+
+    // 배포 환경
+    window.location.href = '/api/oauth2/authorization/kakao';
   };
 
   return (
